847ffb4ce8b53004584a8c3177efb708734e5853
yerdi <yerdi.isman@gmail.com>
2023-01-06 04:53:53 +0600

    4th project finished

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Yerdaulet Ismanaliyev on 05.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
